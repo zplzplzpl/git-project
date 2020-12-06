438adfe79de2e6e40335afc4aa13301258e0bfa4
lisi <lisi@163.com>
2020-12-06 21:23:26 +0800

    创建工程 git

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -2,6 +2,6 @@ package com.bjpowernode.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world nimei");
     }
 }
