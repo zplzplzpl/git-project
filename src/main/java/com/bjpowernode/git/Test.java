ccd0a0007a83c6fcbd9e5aacee9fc95bb78f7dcb
lisi <lisi@163.com>
2020-12-09 19:22:33 +0800

    你想咋地

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -2,6 +2,6 @@ package com.bjpowernode.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world 我又修改了");
+        System.out.println("hello world 我又修改了,你想咋地");
     }
 }
