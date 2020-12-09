1388e406275cc1c733bfb87ada11884beb64f3dd
lisi <lisi@163.com>
2020-12-09 19:12:35 +0800

    再次修改  成功了

--- src/main/java/com/bjpowernode/git/Test.java
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -2,6 +2,6 @@ package com.bjpowernode.git;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world 成功了");
+        System.out.println("hello world 我又修改了");
     }
 }
